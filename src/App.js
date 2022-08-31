d0337b1e09276b4a4532e0cc0dfc13e04b63cc5b
Joseph Coats <coats.joseph30@gmail.com>
2022-08-31 15:41:55 -0700

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName="Joe" lastName="Coats" age={26} hairColor="Black"/>
+      <PersonCard firstName="John" lastName="Doe" age={59} hairColor="Brown"/>
+      <PersonCard firstName="Sarah" lastName="Conner" age={38} hairColor="Blonde"/>
+      <PersonCard firstName="Hannah" lastName="Gayle" age={22} hairColor="Light-Brown"/>
     </div>
   );
 }
