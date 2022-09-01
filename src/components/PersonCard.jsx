5764a35f85d5a7c8cbc11c51b61f4f5d8dbd87de
Joseph Coats <coats.joseph30@gmail.com>
2022-08-31 20:54:23 -0700

    create add age button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+        constructor(props){
+                super(props);
+                        this.state={
+                                age : this.props.age
+                        }
+                }
+        addAge=()=>{
+                this.setState({age:this.state.age+1})
+        }
     render() {
         return (<>
         <h1>{this.props.firstName} {this.props.lastName}</h1>
-        <h5>Age: {this.props.age}</h5>
+        <h5>Age: {this.state.age}</h5>
         <h5>Hair Color: {this.props.hairColor}</h5>
+        <button onClick={this.addAge}>birthday button </button>
         </>);
     }
 }
